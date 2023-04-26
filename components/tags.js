a90374ec27232ada4f5f88289d7ff872f389fd46
Sander Tonheim <sandert.kodehode@gmail.com>
2023-04-26 12:14:05 +0200

    comment out tag element

--- components/tags.js
+++ components/tags.js
@@ -2,17 +2,18 @@ import urlFor from '../lib/sanity'
 import s from '../styles/tags.module.css'
 
 export default function Tags({ list }) {
+	console.log(list)
 	return list.map((tag) => {
 		return (
 			<li
 				className={s.container}
-				key={tag._id}>
-				{tag.Name}
-				<img
-					key={tag._id}
-					src={urlFor(tag.icon.asset._ref)}
+				>
+				{tag?.Name}
+				{/* <img
+					key={tag?._id}
+					src={urlFor(tag?.icon.asset._ref)}
 					alt='picture of user icon'
-				/>
+				/> */}
 			</li>
 		)
 	})
