10782b8fb1133cd7a1c11a81f0887f4d081b2076
Sander Tonheim <sandert.kodehode@gmail.com>
2023-04-26 12:23:33 +0200

    default server func handler

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -62,20 +62,19 @@
 // }
 // data()
 
-import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
-import { Client } from '../../client'
+// import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
 
 const handler = async (req, res) => {
 	//authenticating the webhook
 
 	try {
-		const signature = req.headers[SIGNATURE_HEADER_NAME].toString()
-		if (!isValidSignature(JSON.stringify(req.body), signature, process.env.SANITY_WEBHOOK_SECRET)) return res.status(401).json({ msg: 'Invalid request!' })
+		// const signature = req.headers[SIGNATURE_HEADER_NAME].toString()
+		// if (!isValidSignature(JSON.stringify(req.body), signature, process.env.SANITY_WEBHOOK_SECRET)) return res.status(401).json({ msg: 'Invalid request!' })
 
 		//getting payload
-		const { slug } = req.body
-		await res.revalidate(`/profile`)
-		await res.revalidate(`/profile/${slug}`)
+		// const { slug } = req.body
+		// await res.revalidate(`/profile`)
+		// await res.revalidate(`/profile/${slug}`)
 		res.status(200).json({ msg: 'Product pages revalidated.' })
 	} catch (error) {
 		res.status(500).json({ err: 'Something went Wrong!' })
