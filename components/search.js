31dd91d0a80cb42c48a18d1f32e2526afe278210
Sander Tonheim <sandert.kodehode@gmail.com>
2023-04-28 18:50:33 +0200

    updated search feel and style

--- components/search.js
+++ components/search.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import s from '../styles/search.module.css'
 
 export default function Search() {
-	const [value, setValue] = useState()
+	const [value, setValue] = useState(0)
 	const searchClient = algoliasearch('BC0Z4HS7B1', '7c31f7f4e01eaf32e1ce709e3ec8dd4c')
 	const Hit = ({ hit }) => <Link href={'/profile/' + hit.name.toLowerCase().toString().replace(/\s/g, '-')}>{hit.name}</Link>
 
@@ -13,12 +13,24 @@ export default function Search() {
 		setValue(e.uiState.Members.query)
 	}
 	return (
-		<InstantSearch
-			indexName={'Members'}
-			searchClient={searchClient}
-			onStateChange={onStateChange}>
-			<SearchBox className={s.searchBox} />
-			{value > ' ' ? <Hits hitComponent={Hit} /> : ''}
-		</InstantSearch>
+		<div className={s.container}>
+			<InstantSearch
+				indexName={'Members'}
+				searchClient={searchClient}
+				onStateChange={onStateChange}>
+				<SearchBox
+					className={s.searchBox}
+					placeholder='Søk etter bedrifter'
+				/>
+				{value?.length > 2 ? (
+					<Hits
+						className={s.hits}
+						hitComponent={Hit}
+					/>
+				) : (
+					<></>
+				)}
+			</InstantSearch>
+		</div>
 	)
 }
