01dfd603b3469135a064f02111cd341540ee3f67
Sander Tonheim <sandert.kodehode@gmail.com>
2023-04-28 17:31:24 +0200

    updated component with new algolia pkg

--- components/search.js
+++ components/search.js
@@ -1,42 +1,24 @@
-import algoliasearch from 'algoliasearch'
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom'
-import s from '../styles/search.module.css'
-import { useState } from 'react'
+import algoliasearch from 'algoliasearch/lite'
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-hooks-web'
 import Link from 'next/link'
+import { useState } from 'react'
+import s from '../styles/search.module.css'
 
 export default function Search() {
-	const [value, setValue] = useState(null)
-	const handleChange = (e) => {
-		setValue(e.target.value)
-	}
-
+	const [value, setValue] = useState()
 	const searchClient = algoliasearch('BC0Z4HS7B1', '7c31f7f4e01eaf32e1ce709e3ec8dd4c')
 	const Hit = ({ hit }) => <Link href={'/profile/' + hit.name.toLowerCase().toString().replace(/\s/g, '-')}>{hit.name}</Link>
 
+	const onStateChange = (e) => {
+		setValue(e.uiState.Members.query)
+	}
 	return (
 		<InstantSearch
-			className={s.container}
 			indexName={'Members'}
-			searchClient={searchClient}>
-			<div
-				className={s.search}
-				onChange={handleChange}>
-				<input
-					type='text'
-					placeholder='Søk'
-				/>
-				<button id={s.searchBtn}>søk</button>
-
-				{value > ' ' ? (
-					<Hits
-						hitComponent={Hit}
-						className={s.hit}>
-						{Hits}
-					</Hits>
-				) : (
-					''
-				)}
-			</div>
+			searchClient={searchClient}
+			onStateChange={onStateChange}>
+			<SearchBox className={s.searchBox} />
+			{value > ' ' ? <Hits hitComponent={Hit} /> : ''}
 		</InstantSearch>
 	)
 }
